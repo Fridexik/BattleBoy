59fdb5648d064d19084af6f2294e8d261e53f741
DESKTOP-QM3K7EL\Jakub Fridrich <Fridosek@gmail.com>
2024-02-08 22:56:51 +0100

    add aiming to single target cards

--- scenes/cardUI/card_ui.gd
+++ scenes/cardUI/card_ui.gd
@@ -3,6 +3,8 @@ extends Control
 
 signal reparent_requested(which_card_ui: CardUI)
 
+@export var card:Card
+
 @onready var color: ColorRect = $Color
 @onready var state: Label = $State
 @onready var drop_point_detector = $DropPointDetector
@@ -10,8 +12,15 @@ signal reparent_requested(which_card_ui: CardUI)
 @onready var targets: Array[Node]=[]
 
 
+var parent: Control
+var tween: Tween
+
 func _ready() -> void:
 	card_state_machine.init(self)
+	
+func animate_to_position(new_position: Vector2, duration: float)->void:
+	tween = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self,"global_position", new_position,duration)
 
 func _input(event: InputEvent)->void:
 	card_state_machine.on_input(event)
